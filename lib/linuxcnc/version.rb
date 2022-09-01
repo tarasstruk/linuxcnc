15e31dcbb9fd431f3ecb969b3bc607c0759ebd2d
Taras <t.struk@sonnen.de>
2022-09-01 15:53:28 +0200

    first commit

---
+++ lib/linuxcnc/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Linuxcnc
+  VERSION = "0.1.0"
+end
---
+++ spec/linuxcnc_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Linuxcnc do
+  it "has a version number" do
+    expect(Linuxcnc::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
